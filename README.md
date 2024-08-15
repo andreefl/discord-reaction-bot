7928067e7f2762846e25de32c2db195f2585310c
André Leite <andre_efl@hotmail.com>
2024-08-14 23:03:50 -0300

    fixing configuration validation

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ Requirements:
 - Node v20.7.0
 - Discord bot with Privileged Gateway Intents (https://discord.com/developers/applications)
 
+Remember to create an config file named "reactionsConfig.json" using the template "reactionsConfig.json" before running the bot.
+
 Start bot:
 `npm run start`
 
--- src/utils/validateReactionsConfig.js
+++ src/utils/validateReactionsConfig.js
@@ -1,7 +1,17 @@
+const fs = require('node:fs');
+const path = require('node:path');
 const validateEmoji = require('./validateEmoji');
-const reactionsConfig = require('../config/reactionsConfig.json');
 
 module.exports = () => {
+    const configPath = path.join(__dirname, '../config');
+    const configFiles = fs.readdirSync(configPath).filter(file => file.endsWith('.json') && file.startsWith('reactionsConfig'));
+
+    if ( configFiles.length != 1) {
+        return false;
+    }
+
+    const reactionsConfig = require('../config/reactionsConfig.json');
+
     if (reactionsConfig.anyUser !== Boolean(reactionsConfig.anyUser)) {
         return false;
     }
